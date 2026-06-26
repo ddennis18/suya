405143f85ed80cb022f8fb7598231acf6f2a0c78
ddennis18 <osemwonken18@gmail.com>
2026-06-27 00:37:05 +0100

    fix: flipped the command line borad display

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -6,8 +6,9 @@
 
 std::string Board::toString() {
   std::string ret = "\n";
-  for (auto & rank : squares) {
-    for (auto p : rank) {
+  for (int i =7;i >= 0; i--) {
+    for (int j = 7; j >= 0 ; j--) {
+      int p = squares[i][j];
       const int color = p & 0b1000;
       const int type = p & 0b0111;
 
