3b08f217f6c52c38b46dd4f5deeeea24c98f92fe
ddennis18 <osemwonken18@gmail.com>
2026-06-28 21:50:26 +0100

    feat: added knight movement functionality

--- src/movegen/MoveGenerator.cpp
+++ src/movegen/MoveGenerator.cpp
@@ -46,9 +46,10 @@ std::list<Move> generateMoves(const Board &b) {
         int leftCapture = 8 * i + j + direction * 7;
         int rightCapture = 8 * i + j + direction * 9;
 
-        //j!=0 for the a file since it cant capture to the left whilst on the a file
+        //j!=7 for the a file since it cant capture to the left whilst on the a file
         if ((j != 7) && withinBoard(leftCapture) && !b.isEmpty(leftCapture) &&
-            (pieceColor(b.getSquare(leftCapture)) != workingColor)) {
+            (pieceColor(b.getSquare(leftCapture)) != workingColor)
+            && (pieceType(b.getSquare(leftCapture)) != KING)) {
           int capturedPiece = b.getSquare(leftCapture);
           Move m{
             .start = 8 * i + j, .target = leftCapture,
@@ -58,9 +59,10 @@ std::list<Move> generateMoves(const Board &b) {
           moveList.push_back(m);
         }
 
-        //j!=7 for the h file since it cant capture to the right whilst on the h file
+        //j!=0 for the h file since it cant capture to the right whilst on the h file
         if ((j != 0) && withinBoard(rightCapture) && !b.isEmpty(rightCapture) &&
-            (pieceColor(b.getSquare(rightCapture)) != workingColor)) {
+            (pieceColor(b.getSquare(rightCapture)) != workingColor)
+            && (pieceType(b.getSquare(rightCapture)) != KING)) {
           int capturedPiece = b.getSquare(rightCapture);
           Move m{
             .start = 8 * i + j, .target = rightCapture,
@@ -68,6 +70,41 @@ std::list<Move> generateMoves(const Board &b) {
           };
 
           moveList.push_back(m);
+        }
+      } else if (type == KNIGHT) {
+        //squares the knight can move to relative to its position
+        std::array<std::array<int, 2>, 8> relativeSquares = {
+          2, 1,
+          2, -1,
+          1, 2,
+          -1, 2,
+          1, -2,
+          -1, -2,
+          -2, 1,
+          -2, -1
+        };
+
+        for (auto rs: relativeSquares) {
+          rs[0] += i;
+          rs[1] += j;
+          int p = rs[0] * 8 + rs[1];
+
+          //skip the square if its outside the board
+          //NOTE i didnt use the single number position to check within board because that wraps
+          //around the board which i dont want
+          if (!withinBoard(rs[0], rs[1])) { continue; }
+
+          int square = b.getSquare(p);
+          if (square == EMPTY) {
+            Move m = {.start = 8 * i + j, .target = p, .piece = piece};
+            moveList.push_back(m);
+          } else if ((pieceColor(square) != workingColor) && (pieceType(square) != KING)) {
+            Move m = {
+              .start = 8 * i + j, .target = p, .piece = piece,
+              .captures = true, .capturedPiece = square
+            };
+            moveList.push_back(m);
+          }
         }
       }
     }
