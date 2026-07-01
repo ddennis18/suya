12cb4eb2521e420434adb75133f022692e050d87
ddennis18 <osemwonken18@gmail.com>
2026-07-01 23:47:48 +0100

    feat: the main tests the movegen in the opening setup and output the moves

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,23 @@
 #include "utils/utils.h"
 
 int main() {
-  std::string fen = "";
-  std::getline(std::cin, fen);
-  Board b = convertFEN(fen);
-  std::cout<<b.toString();
+  // std::string fen = "";
+  // std::getline(std::cin, fen);
+  // Board b = convertFEN(fen);
+
+  Board b = convertFEN("rnbqkbnr/pppppppp//8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  std::cout << b.toString();
 
   auto moves = generateMoves(b);
-  std::cout<<"done";
+  std::cout << "Total Number of Possible Moves: " << moves.lenght() << '\n';
+  for (int i = 0; i < moves.lenght(); i++) {
+    Move move = moves[i];
+    std::cout << pieceTypeStringTable[pieceType(move.piece)] << ": " << move.start << "->" << move.target;
+    if (move.captures) {
+      std::cout << " captures: " << move.capturedPiece;
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << "done" << '\n';
 }
